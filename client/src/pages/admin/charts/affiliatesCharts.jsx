c6e15f448d602a49a5f60a66a2b20ce5ad0e7d0a
kennedymakokha <katchibo2@gmail.com>
2025-02-01 07:56:43 +0300

    fixing imports

--- client/src/pages/admin/charts/affiliatesCharts.jsx
+++ client/src/pages/admin/charts/affiliatesCharts.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 import { useFetchaffiliatesQuery } from '../../../features/slices/usersApiSlice';
